d0a5c9de1446bf96db79b9d1b564d4608f69dad1
booogie-man-07 <booogie.man.07@gmail.com>
2022-07-17 00:16:24 +0300

    Rest vacation response added.

---
+++ VacationInfo.php
@@ -0,0 +1,48 @@
+﻿<?Php
+
+class VacationInfo {
+
+    var email = null;
+
+    function __construct($email) {
+        $this->email = $email;
+    }
+
+    function getVacationInfo() {
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "80",
+            CURLOPT_URL => "http://192.168.1.20/DA_ERP/hs/Staff/StaffData?EMAIL=${$this->email}&DATA=bot",
+            CURLOPT_USERPWD => "Web1C:67z%Cc#2",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 300,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "Content-Type: application/json"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            //echo "cURL Error #: ".$err;
+            return "Извините, информация по количеству оставшегося отпуска недоступна, попробуйте запросить позднее";
+        } else {
+
+            //$result = json_decode($response, true);
+            //echo $response;
+            return (string) $response;
+
+        }
+    }
+}
+
+?>
