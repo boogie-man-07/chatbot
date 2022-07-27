62f389bd4b3fcd9824d375bbdc8442770828c151
booogie-man-07 <booogie.man.07@gmail.com>
2022-07-28 00:27:31 +0300

    Postpone vacation - sending forms step.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -587,7 +587,7 @@ class AuthorizedUserScenario {
                 $month = $date->format("F");
                 $year = $date->format("Y");
                 foreach ($separatedVacationFormData as $vacation) {
-                    sendMessage($this->chatID, $separatedVacationFormData['startdate'], null);
+                    sendMessage($this->chatID, $vacation['startdate'], null);
 //                     $this->forms->getPostponeVacationForm($this->user['position'], $this->user['form_fullname'], $vacationFormData['startdate'], $vacationFormData['enddate'], $vacation['startdate'], $vacation['enddate'], $vacation['reason'], $day, $month, $year, $sign, $this->user['company_id']);
 //                     $template = $this->email->generatePostponeVacationForm($this->user['company_id']);
 //                     $template = str_replace("{firstname}", $this->user['firstname'], $template);
