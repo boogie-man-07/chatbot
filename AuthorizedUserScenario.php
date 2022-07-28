0f81a1465eb2743b256a51da5a5be9724ab1b7da
booogie-man-07 <booogie.man.07@gmail.com>
2022-07-29 00:20:42 +0300

    Postpone vacation - sending forms step.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -587,17 +587,17 @@ class AuthorizedUserScenario {
                 $month = $date->format("F");
                 $year = $date->format("Y");
                 foreach ($separatedVacationFormData as $vacation) {
-                    $template = null;
-                    //sendMessage($this->chatID, $vacationFormData['startdate'], null);
+                    //$template = null;
                     $this->forms->getPostponeVacationForm($this->user['position'], $this->user['fullname'], $vacationFormData['startdate'], $vacationFormData['enddate'], $vacation['startdate'], $vacation['enddate'], $vacation['reason'], $day, $month, $year, $sign, $this->user['company_id']);
                     $template = $this->email->generatePostponeVacationForm($this->user['company_id']);
                     $template = str_replace("{firstname}", $this->user['firstname'], $template);
-                    $this->swiftmailer->sendPostponedVacationMailWithAttachementViaSmtp(
-                        $this->user['company_id'],
-                        $this->user['email'],
-                        "Образец заявления на перенос отпуска",
-                        $template
-                    );
+//                     $this->swiftmailer->sendPostponedVacationMailWithAttachementViaSmtp(
+//                         $this->user['company_id'],
+//                         $this->user['email'],
+//                         "Образец заявления на перенос отпуска",
+//                         $template
+//                     );
+                    sendMessage($this->chatID, $vacationFormData['startdate'], null);
                 }
 
                 $this->access->setState($this->chatID, $this->states['authorizationCompletedState']);
