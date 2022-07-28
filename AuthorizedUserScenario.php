144e705bc9012822d2b7e3804da60ef7fdda2296
booogie-man-07 <booogie.man.07@gmail.com>
2022-07-28 22:21:03 +0300

    Postpone vacation - sending forms step.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -588,15 +588,15 @@ class AuthorizedUserScenario {
                 $year = $date->format("Y");
                 foreach ($separatedVacationFormData as $vacation) {
                     sendMessage($this->chatID, $vacation['startdate'], null);
-//                     $this->forms->getPostponeVacationForm($this->user['position'], $this->user['form_fullname'], $vacationFormData['startdate'], $vacationFormData['enddate'], $vacation['startdate'], $vacation['enddate'], $vacation['reason'], $day, $month, $year, $sign, $this->user['company_id']);
-//                     $template = $this->email->generatePostponeVacationForm($this->user['company_id']);
-//                     $template = str_replace("{firstname}", $this->user['firstname'], $template);
-//                     $this->swiftmailer->sendPostponedVacationMailWithAttachementViaSmtp(
-//                         $this->user['company_id'],
-//                         $this->user['email'],
-//                         "Образец заявления на перенос отпуска",
-//                         $template
-//                     );
+                    $this->forms->getPostponeVacationForm($this->user['position'], $this->user['form_fullname'], $vacationFormData['startdate'], $vacationFormData['enddate'], $vacation['startdate'], $vacation['enddate'], $vacation['reason'], $day, $month, $year, $sign, $this->user['company_id']);
+                    $template = $this->email->generatePostponeVacationForm($this->user['company_id']);
+                    $template = str_replace("{firstname}", $this->user['firstname'], $template);
+                    $this->swiftmailer->sendPostponedVacationMailWithAttachementViaSmtp(
+                        $this->user['company_id'],
+                        $this->user['email'],
+                        "Образец заявления на перенос отпуска",
+                        $template
+                    );
                 }
 
                 $this->access->setState($this->chatID, $this->states['authorizationCompletedState']);
