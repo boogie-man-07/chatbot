aed964ae1e4e5b53e8652d898cbc44cbfe707b13
booogie-man-07 <booogie.man.07@gmail.com>
2022-07-30 14:51:09 +0300

    Postpone vacation - sending forms step.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -587,9 +587,9 @@ class AuthorizedUserScenario {
                 $month = $date->format("F");
                 $year = $date->format("Y");
                 foreach ($separatedVacationFormData as $value) {
-                    $this->forms->getPostponeVacationForm($this->user['position'], $this->user['fullname'], $vacationFormData['startdate'], $vacationFormData['enddate'], $value['startdate'], $value['enddate'], $value['reason'], $day, $month, $year, $sign, $this->user['company_id']);
-//                     $template = $this->email->generatePostponeVacationForm($this->user['company_id']);
-//                     $template = str_replace("{firstname}", $this->user['firstname'], $template);
+//                     $this->forms->getPostponeVacationForm($this->user['position'], $this->user['fullname'], $vacationFormData['startdate'], $vacationFormData['enddate'], $value['startdate'], $value['enddate'], $value['reason'], $day, $month, $year, $sign, $this->user['company_id']);
+                    $template = $this->email->generatePostponeVacationForm($this->user['company_id']);
+                    $template = str_replace("{firstname}", $this->user['firstname'], $template);
 //                     $this->swiftmailer->sendPostponedVacationMailWithAttachementViaSmtp(
 //                         $this->user['company_id'],
 //                         'booogie.man.07@gmail.com',
