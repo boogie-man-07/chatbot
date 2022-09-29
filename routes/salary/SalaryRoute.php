7a5f9a108f40f3d8371736efdedd6ddec41be370
booogie-man-07 <booogie.man.07@gmail.com>
2022-09-29 14:45:34 +0300

    Rest vacations count fixed.

--- routes/salary/SalaryRoute.php
+++ routes/salary/SalaryRoute.php
@@ -40,7 +40,7 @@ class SalaryRoute {
         $data = $vacationInfo->getRestVacationCountNew($email);
         $vacations = $vacationInfo->getVacationsInfo($email);
         $reply = $this->constants->getRestVacationInfoText($data, $vacations);
-        sendMessage($chatID, $reply, null);
+        sendMessage($chatID, $data, null);
     }
 
     function triggerActionForChooseVacationToPostpone($chatID, $data, $firstname) {
