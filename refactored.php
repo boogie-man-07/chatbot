006d5d329d1e9df3b42efe7df61d17bbcf689503
booogie-man-07 <booogie.man.07@gmail.com>
2022-10-18 15:32:33 +0300

    Check calendar.

--- refactored.php
+++ refactored.php
@@ -63,7 +63,7 @@ $updates = file_get_contents('php://input');
 $updates = json_decode($updates, true);
 $phoneNumber = $updates['message']['contact']['phone_number'];
 $query = $updates["callback_query"];
-$message = $queryData ? $query['message'] : $updates['message'];
+$messageId = $queryData ? $updates["inline_query"]['id'] : $updates['message']['message_id'];
 $queryID = $query["id"];
 $queryData = $query["data"];
 
@@ -90,7 +90,7 @@ if (!$user) {
             $nonFinishedAuthorizationUserScenario->run($text);
         }
     } else {
-        $authorizedUserScenario = new AuthorizedUserScenario($chatID, $user, $username, $access, $swiftmailer, $authroute, $commonmistakeroute, $phonebookroute, $valuesRoute, $mainRulesRoute, $mainInformationRoute, $salaryRoute, $commandList, $statesList, $state, $logics, $forms, $email, $vacationInfo, $query, $logs, $message);
+        $authorizedUserScenario = new AuthorizedUserScenario($chatID, $user, $username, $access, $swiftmailer, $authroute, $commonmistakeroute, $phonebookroute, $valuesRoute, $mainRulesRoute, $mainInformationRoute, $salaryRoute, $commandList, $statesList, $state, $logics, $forms, $email, $vacationInfo, $query, $logs, $messageId);
         if ($isInline) {
             $authorizedUserScenario->runInline($text);
         } else {
