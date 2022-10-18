b8a715609be5963ef17b70d270e2b8a2c7afb738
booogie-man-07 <booogie.man.07@gmail.com>
2022-10-18 14:47:45 +0300

    Check calendar.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -448,7 +448,7 @@ class AuthorizedUserScenario {
         switch ($text) {
             // remove
             case $this->commands['calendarInline']:
-                sendMessage($this->chatId, $this->query['id'], null);
+                sendMessage($this->chatId, (string)$this->query['id'], null);
 //                 $keyboard = json_encode(array(
 //                     "inline_keyboard" => array(
 //                         array(
