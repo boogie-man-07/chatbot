85fa61122b4ad52195ba42bd34c1b548f908d88f
booogie-man-07 <booogie.man.07@gmail.com>
2022-10-18 14:05:22 +0300

    Check calendar.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -451,7 +451,7 @@ class AuthorizedUserScenario {
         switch ($text) {
             // remove
             case $this->commands['calendarInline']:
-                sendMessage($this->chatID, json_encode($this->query), null);
+                sendMessage($this->chatID, $this->query, null);
                 exit;
             case $this->commands['userFullCardInline']:
                 $userForFind = $this->access->getFindUserData($this->chatID);
