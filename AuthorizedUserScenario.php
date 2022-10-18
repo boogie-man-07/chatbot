c46010588015672e26e82dedcca5ba921b4d7f09
booogie-man-07 <booogie.man.07@gmail.com>
2022-10-18 14:54:52 +0300

    Check calendar.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -59,7 +59,7 @@ class AuthorizedUserScenario {
             // remove
             case $this->commands['calendar']:
                 $this->salaryRoute->triggerCalendarAction($this->chatID);
-                sendMessage($this->chatId, json_encode($updates), null);
+                sendMessage($this->chatID, json_encode($updates), null);
                 exit;
             case $this->commands['start']:
                 $this->access->setState($this->chatID, $this->states['authorizationCompletedState']);
@@ -451,7 +451,7 @@ class AuthorizedUserScenario {
         switch ($text) {
             // remove
             case $this->commands['calendarInline']:
-//                 sendMessage($this->chatId, (string)$updates, null);
+                sendMessage($this->chatID, json_encode($updates), null);
 //                 $keyboard = json_encode(array(
 //                     "inline_keyboard" => array(
 //                         array(
