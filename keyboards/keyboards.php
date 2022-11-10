9834c3c2969e431bcaaecf6f3f805b41250d506a
booogie-man-07 <booogie.man.07@gmail.com>
2022-11-11 02:16:05 +0300

    Diall polls implementation.

--- keyboards/keyboards.php
+++ keyboards/keyboards.php
@@ -520,7 +520,7 @@ class keyboards {
 
     function getInlineKeyboardForAskADmsPollQuestion($pollQuestionInfo) {
         $replyList = array();
-        foreach($pollQuestionInfo['reply_options'] $value) {
+        foreach($pollQuestionInfo['reply_options'] as $value) {
             $itemTitle = $value['title'];
             $callback_data = $value['pol_id']."_".$value['question_id']."_".$value['title'];
             $replyItem = array(array(
