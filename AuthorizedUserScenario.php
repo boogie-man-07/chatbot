513df256b5217412ae21160d164c83aab8833021
booogie-man-07 <booogie.man.07@gmail.com>
2022-11-12 20:37:51 +0300

    Diall poll update.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -780,9 +780,8 @@ class AuthorizedUserScenario {
                 $pollInfo = $this->access->getDmsPollInfo($this->user['user_id']);
                 $pollQuestionInfo = $this->access->getDmsPollQuestionsInfo(1);
                 $this->access->setState($this->chatID, $this->states['dmsPoolReplyWaitingState'], true);
-                //$this->salaryRoute->triggerActionForAskDmsPollQuestion($this->chatID, $this->user['user_id'], $pollInfo, $pollQuestionInfo, false);
+                $this->salaryRoute->triggerActionForAskDmsPollQuestion($this->chatID, $this->user['user_id'], $pollInfo, $pollQuestionInfo, false);
                 answerCallbackQuery($this->query["id"], "Вопрос загружен!");
-                sendMessage($this->chatID, $pollQuestionInfo[0]['question_text'], null);
                 exit;
             case $this->commands['sendDmsQuestionInline']:
                 $questionInfo = $this->access->getDmsQuestionInfo($this->chatID);
