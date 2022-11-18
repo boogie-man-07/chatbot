ba06d35c11caff89089176da20a1ba84e611807e
booogie-man-07 <booogie.man.07@gmail.com>
2022-11-18 16:29:58 +0300

    Diall employee calendar.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -855,7 +855,7 @@ class AuthorizedUserScenario {
                 exit;
             case $this->commands['nextMonthCalendarInline']:
                 $offset = $this->access->getCalendarOffset($this->user['user_id']);
-                sendMessage($this->chatID, json_encode($offset), null);
+                sendMessage($this->chatID, json_encode($offset[0]), null);
 //                 $nextMonth = $this->salaryRoute->getNextMonth($offset['offset']);
 //                 $nextOffset = $this->salaryRoute->generateNextOffset($offset['offset']);
 //                 $offset = $this->access->setCalendarOffset($this->user['userId'], $nextOffset);
@@ -873,7 +873,7 @@ class AuthorizedUserScenario {
                             exit;
                         }
                     default:
-                        sendMessage($this->chatID, "Default finished inline", null);
+                        answerCallbackQuery($this->query["id"], "Default finished inline");
                         exit;
                 }
         }
