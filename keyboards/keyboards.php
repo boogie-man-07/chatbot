70f2bbdff43f8a61f71574453cc5866eb4caf7b0
booogie-man-07 <booogie.man.07@gmail.com>
2022-11-19 01:56:24 +0300

    Diall employee calendar.

--- keyboards/keyboards.php
+++ keyboards/keyboards.php
@@ -567,22 +567,20 @@ class keyboards {
         $c = 0;
         $count = 0;
         for ($i = 0; $i < $startCell; $i++) {
-            $count++;
             array_push($firstRowArray, array("text" => " ", "callback_data" => "defaultCallbackResponse"));
-
+            $count++;
         }
 
         for ($m = $startCell; $m < 7; $m++) {
-            $count++;
             array_push($firstRowArray, array(
                 "text" => (string)$daysList[$c]['buttonText'],
                 "callback_data" => "defaultCallbackResponse")
             );
             $c++;
-
+            $count++;
         }
-//         array_push($mainArray, $firstRowArray);
-//
+        array_push($mainArray, $firstRowArray);
+
 //         while ($count < 7) {
 //             $rowArray = array();
 //             while ($count < 7) {
@@ -595,8 +593,8 @@ class keyboards {
 //             }
 //             array_push($mainArray, $rowArray);
 //         }
-//
-//         return $mainArray;
+
+        return $mainArray;
     }
 
     function getCalendar($month) {
