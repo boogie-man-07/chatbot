a6579b4a9e6fe455f00df172d036aa0df22ded04
booogie-man-07 <booogie.man.07@gmail.com>
2022-11-24 00:13:49 +0300

    Diall poll increase questions types.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -905,7 +905,7 @@ class AuthorizedUserScenario {
                                 $this->access->setSelectedDmsPollOption($this->user['user_id'], $text);
                             } else if ($pollQuestionInfo[$id]['question_type'] == 2) {
                                 $this->access->setSelectedDmsPollOptionForMultipleChoose($this->user['user_id'], $text, $pollInfo, $pollQuestionInfo);
-                                sendMessage($this->chatID, 'after save response', null);
+                                sendMessage($this->chatID, $text, null);
                                 exit;
                             } else if ($pollQuestionInfo[$id]['question_type'] == 3) {
 
