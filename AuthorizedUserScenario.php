702eac360bea1863e6bf9393b5334e5727138148
booogie-man-07 <booogie.man.07@gmail.com>
2022-11-24 18:07:36 +0300

    Diall poll increase questions types.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -508,6 +508,7 @@ class AuthorizedUserScenario {
                                     sendMessage($this->chatID, "case not-inline 3-1", null);
                                     $this->access->setSelectedDmsPollOptionForFreeReply($this->user['user_id'], $text, $pollInfo, $pollQuestionInfo);
                                 } else if ($pollQuestionInfo[$id]['question_type'] == 4) {
+                                    $this->access->setSelectedDmsPollOption($this->user['user_id'], $text);
                                     sendMessage($this->chatID, "case not-inline 4-1", null);
                                 }
 
@@ -962,6 +963,7 @@ class AuthorizedUserScenario {
                                 answerCallbackQuery($this->query["id"], "case inline 3-1");
                                 $this->access->setSelectedDmsPollOptionForFreeReply($this->user['user_id'], $text, $pollInfo, $pollQuestionInfo);
                             } else if ($pollQuestionInfo[$id]['question_type'] == 4) {
+                                $this->access->setSelectedDmsPollOption($this->user['user_id'], $text);
                                 answerCallbackQuery($this->query["id"], "case inline 4-1");
                             }
 
