1f6913a733a4ced5929e5ee7431acc9f4980d69c
booogie-man-07 <booogie.man.07@gmail.com>
2022-11-29 20:43:37 +0300

    Diall hrlink integration preparations.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -146,8 +146,8 @@ class AuthorizedUserScenario {
                 $pollInfo = $this->access->getDmsPollInfo($this->user['user_id']);
                 $this->salaryRoute->triggerActionForShowDmsMenu($this->chatID, $this->user['firstname'], $this->user['dms_type'], $pollInfo['is_finished']);
                 // to delete
-                $applicationsList = $this->hrLinkApiProvider->getApplicationTypes();
-                sendMessage($this->chatID, $applicationsList['applicationTypes'][0]['name'], null);
+//                 $applicationsList = $this->hrLinkApiProvider->getApplicationTypes();
+//                 sendMessage($this->chatID, $applicationsList['applicationTypes'][0]['name'], null);
                 exit;
             case $this->commands['dmsMemo']:
                 $this->salaryRoute->triggerActionForSendDmsMemo($this->chatID, $this->user['dms_type']);
