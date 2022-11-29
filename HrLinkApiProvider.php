684c2f71db6cc0644fe455adb77410afd6885064
booogie-man-07 <booogie.man.07@gmail.com>
2022-11-29 20:29:40 +0300

    Diall hrlink integration preparations.

--- HrLinkApiProvider.php
+++ HrLinkApiProvider.php
@@ -6,24 +6,20 @@ class HrLinkApiProvider {
 
         $curl = curl_init();
         curl_setopt_array($curl, array(
-            CURLOPT_PORT => "443",
-            CURLOPT_URL => "https://hrlink.diall.ru/api/v1/applicationTypes",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                "cache-control: no-cache",
-                "Content-Type: application/json",
-                "User-Api-Token: 6255b66b-33c6-424d-8d7f-e85b9e5fc162"
-            ),
+          CURLOPT_URL => 'https://hrlink.diall.ru/api/v1/applicationTypes',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+          CURLOPT_HTTPHEADER => array(
+            'User-Api-Token: 6255b66b-33c6-424d-8d7f-e85b9e5fc162'
+          ),
         ));
 
         $response = curl_exec($curl);
-        $err = curl_error($curl);
-
         curl_close($curl);
 
         if ($err) {
