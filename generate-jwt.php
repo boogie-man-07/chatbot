2c78470f6fc1f54c8e490110c0dbb5aeb5e0cbf8
booogie-man-07 <booogie.man.07@gmail.com>
2022-12-16 16:39:32 +0300

    Diall hr-link jwt library added.

--- generate-jwt.php
+++ generate-jwt.php
@@ -5,37 +5,44 @@ include ('vendor/autoload.php');
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
-$pk = file_get_contents('/var/www/sigmabot.ddns.net/pk.key');
-$pubk = file_get_contents('/var/www/sigmabot.ddns.net/pubk.pub');
+generateBearerToken();
 
-$privateKey = <<<EOD
-$pk
-EOD;
 
-$publicKey = <<<EOD
-$pubk
-EOD;
+function generateBearerToken() {
+    $pk = file_get_contents('/var/www/sigmabot.ddns.net/pk.key');
+    $pubk = file_get_contents('/var/www/sigmabot.ddns.net/pubk.pub');
 
-$payload = [
-    'iss' => 'DiallAlianceT',
-    'sub' => '108da251-6c21-4105-80f2-99386f97a313',
-    'aud' => 'esa.hr-link.ru',
-    'exp' => time() + (60 * 5),
-    'nbf' => time(),
-    'iat' => time()
-];
+    $privateKey = <<<EOD
+    $pk
+    EOD;
 
-$jwt = JWT::encode($payload, $privateKey, 'RS256');
-echo "Encode:\n" . print_r($jwt, true) . "\n";
+    $publicKey = <<<EOD
+    $pubk
+    EOD;
 
-// $decoded = JWT::decode($jwt, new Key($publicKey, 'RS256'));
+    $payload = [
+        'iss' => 'DiallAlianceT',
+        'sub' => '108da251-6c21-4105-80f2-99386f97a313',
+        'aud' => 'esa.hr-link.ru',
+        'exp' => time() + (60 * 5),
+        'nbf' => time(),
+        'iat' => time()
+    ];
 
-/*
- NOTE: This will now be an object instead of an associative array. To get
- an associative array, you will need to cast it as such:
-*/
+    $jwt = JWT::encode($payload, $privateKey, 'RS256');
+    echo "Encode:\n" . print_r($jwt, true) . "\n";
 
-// $decoded_array = (array) $decoded;
-// echo "Decode:\n" . print_r($decoded_array, true) . "\n";
+    // $decoded = JWT::decode($jwt, new Key($publicKey, 'RS256'));
+
+    /*
+     NOTE: This will now be an object instead of an associative array. To get
+     an associative array, you will need to cast it as such:
+    */
+
+    // $decoded_array = (array) $decoded;
+    // echo "Decode:\n" . print_r($decoded_array, true) . "\n";
+}
+
+
 
 ?>
