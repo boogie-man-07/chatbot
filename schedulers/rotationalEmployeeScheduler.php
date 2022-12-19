ae3e4d75ac8751125b8579689f003c8736afe95e
booogie-man-07 <booogie.man.07@gmail.com>
2022-12-19 21:50:44 +0300

    Diall job for rotational workers added. Job for users corrected.

--- schedulers/rotationalEmployeeScheduler.php
+++ schedulers/rotationalEmployeeScheduler.php
@@ -19,7 +19,9 @@ $offset = "0";
 
 $user = $access->getUserByChatID($chatID);
 
-echo "currentMonth: ".$currentMonth."<br><br>";
+
+
+
 
 echo "today: ".date('d.m.Y')."<br><br>";
 
@@ -34,33 +36,5 @@ echo "first day of founded month: ".$firstDay."<br><br>";
 $rotationalWorkerInfo = $calendarInfo->getMonthlyData($user['user_id'], $firstDay, $offset);
 
 echo json_encode($rotationalWorkerInfo);
-
-function getCurrentMonth() {
-    $firstDay = strtotime('first day of this month', time());
-    return date('d.m.Y', $firstDay);
-}
-
-function getNextMonth($offset) {
-    $firstDay = strtotime("first day of $offset month", time());
-    return date('d.m.Y', $firstDay);
-}
-
-
-echo "currentMonth: ".$currentMonth."<br><br>";
-
-echo "today: ".date('d.m.Y')."<br><br>";
-
-$timestamp = strtotime(date('d.m.Y'));
-$totalTimeStamp = strtotime('+ 3 days', $timestamp);
-echo "finding day :".date('d.m.Y', $totalTimeStamp)."<br><br>";
-
-$firstDay = date('01.m.Y', $totalTimeStamp);
-
-echo "first day of founded month: ".$firstDay."<br><br>";
-
-function getCurrentMonth() {
-    $firstDay = strtotime('first day of this month', time());
-    return date('d.m.Y', $firstDay);
-}
 
 ?>
