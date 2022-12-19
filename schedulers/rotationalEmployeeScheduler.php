391b571a9efe743010329bbc9680519f98b30847
booogie-man-07 <booogie.man.07@gmail.com>
2022-12-20 01:25:21 +0300

    Diall job for rotational workers added. Job for users corrected.

--- schedulers/rotationalEmployeeScheduler.php
+++ schedulers/rotationalEmployeeScheduler.php
@@ -1,5 +1,6 @@
 <?Php
 
+// Инициализируем переменные необходимых классов и подключение к БД
 $file = parse_ini_file("../../../Testbotdb.ini");
 
 $host = trim($file["dbhost"]);
@@ -14,57 +15,75 @@ $access->connect();
 require ("../CalendarInfo.php");
 $calendarInfo = new CalendarInfo();
 
-$chatID = '187967374';
+// Берем из БД всех сотрудников, которые работают вахтовым методом + зарегистрированы в боте
+$rotationalWorkersListFromDb = $access->getRotationalWorkersListFromDb();
+// $chatID = '187967374';
+// $user = $access->getUserByChatID($chatID);
 $offset = "0";
 
-$user = $access->getUserByChatID($chatID);
-
-
-
-
-
-echo "today: ".date('d.m.Y')."<br><br>";
-
-$timestamp = strtotime(date('d.m.Y'));
-$totalTimeStamp = strtotime('+ 3 days', $timestamp);
-$findingDate = date('d.m.Y', $totalTimeStamp);
+$currentTimestamp = strtotime(date('d.m.Y'));
+$desiredTimestamp = strtotime('+ 3 days', $currentTimestamp);
+$desiredDate = date('d.m.Y', $desiredTimestamp);
 echo "finding day :".$findingDate."<br><br>";
+// $desiredDateFirstDayOfMonth = date('01.m.Y', $desiredTimestamp);
+// echo "first day of founded month: ".$firstDayDate."<br><br>";
 
-$firstDayDate = date('01.m.Y', $totalTimeStamp);
+//************
 
-echo "first day of founded month: ".$firstDayDate."<br><br>";
+foreach ($rotationalWorkersListFromDb as $rotationalWorkerFromDb) {
+    $rotationalWorkerInfo = $calendarInfo->getMonthlyData($rotationalWorkerFromDb['user_id'], $desiredDateFirstDayOfMonth, $offset);
+    $dateNumber = substr($desiredDate, 0, 1) == "0" ? substr(substr($desiredDate, 0, 2), 1) : substr($desiredDate, 0, 2);
 
-$rotationalWorkerInfo = $calendarInfo->getMonthlyData($user['user_id'], $firstDayDate, $offset);
-
-echo json_encode($rotationalWorkerInfo)."<br><br>";
-
-$dateNumber = substr($findingDate, 0, 1) == "0" ? substr(substr($findingDate, 0, 2), 1) : substr($findingDate, 0, 2);
-echo $dateNumber."<br><br>";
-
-foreach ($rotationalWorkerInfo['daysList'] as $key=>$value) {
-    if ($dateNumber > 1) {
-        if ($value['dateNumber'] === $dateNumber) {
-            echo $key;
-            echo "<br><br>";
-            echo json_encode($rotationalWorkerInfo['daysList'][$key - 1]);
-            echo "<br><br>";
-            echo json_encode($value)."<br><br>";
-            echo "Рабочий день: <br><br>";
-            $previous = $rotationalWorkerInfo['daysList'][$key - 1]['isWorkingDay'] ? true : false;
-            $finding = $value['isWorkingDay'] ? true : false;
-
-            $shouldSendNotification = (!$previous && $finding);
-            echo "Предыдущий: ".$previous."<br><br>";
-            echo "Искомый: ".$finding."<br><br>";
-            echo "Рабочий ли день?: ".$shouldSendNotification."<br><br>";
-
-            if ($finding && !$previous) {
-                echo "День рабочий, нужно отправить уведомление.<br><br>";
-            } else {
-                echo "День не рабочий, не нужно отправлять уведомление.<br><br>";
+    foreach ($rotationalWorkerInfo['daysList'] as $key=>$value) {
+        if ($dateNumber > 1) {
+            if ($value['dateNumber'] === $dateNumber) {
+                $targetDay = $value['isWorkingDay'] ? true : false;
+                $previousDay = $rotationalWorkerInfo['daysList'][$key - 1]['isWorkingDay'] ? true : false;
+                if ($targetDay && !$previousDay) {
+                    echo "Для работника ".$rotationalWorkerFromDb['fullname']." день ".$desiredDate." является днем начала ближайшей вахты, необходимо отправить уведомление для telegramId: ".$rotationalWorkerFromDb['tg_chat_id']."<br><br>";
+                } else {
+                    echo "Для работника ".$rotationalWorkerFromDb['fullname']." день ".$desiredDate." не является днем начала ближайшей вахты, отправлять уведомление для telegramId: ".$rotationalWorkerFromDb['tg_chat_id']." не нужно.<br><br>";
+                }
             }
         }
     }
 }
+
+//************
+
+
+
+// $rotationalWorkerInfo = $calendarInfo->getMonthlyData($user['user_id'], $firstDayDate, $offset);
+//
+// echo json_encode($rotationalWorkerInfo)."<br><br>";
+//
+// $dateNumber = substr($findingDate, 0, 1) == "0" ? substr(substr($findingDate, 0, 2), 1) : substr($findingDate, 0, 2);
+// echo $dateNumber."<br><br>";
+//
+// foreach ($rotationalWorkerInfo['daysList'] as $key=>$value) {
+//     if ($dateNumber > 1) {
+//         if ($value['dateNumber'] === $dateNumber) {
+//             echo $key;
+//             echo "<br><br>";
+//             echo json_encode($rotationalWorkerInfo['daysList'][$key - 1]);
+//             echo "<br><br>";
+//             echo json_encode($value)."<br><br>";
+//             echo "Рабочий день: <br><br>";
+//             $previous = $rotationalWorkerInfo['daysList'][$key - 1]['isWorkingDay'] ? true : false;
+//             $finding = $value['isWorkingDay'] ? true : false;
+//
+//             $shouldSendNotification = (!$previous && $finding);
+//             echo "Предыдущий: ".$previous."<br><br>";
+//             echo "Искомый: ".$finding."<br><br>";
+//             echo "Рабочий ли день?: ".$shouldSendNotification."<br><br>";
+//
+//             if ($finding && !$previous) {
+//                 echo "Это первый рабочий день, нужно отправить уведомление.<br><br>";
+//             } else {
+//                 echo "Это не первый рабочий день, не нужно отправлять уведомление.<br><br>";
+//             }
+//         }
+//     }
+// }
 
 ?>
