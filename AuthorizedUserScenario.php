7678a1812720eb59ea1bdd856dbdb32212fb762e
booogie-man-07 <booogie.man.07@gmail.com>
2022-12-27 19:16:18 +0300

    Diall HRLink integration.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -961,7 +961,7 @@ class AuthorizedUserScenario {
                 $formData = $this->access->getIssuingDocumentData($this->user['user_id']);
                 $bossPhysicalId = $this->access->getBossPhysicalId($this->user['boss']);
                 $applicationInfo = $this->access->getApplicationIdsInfo($formData['issue_type']);
-                sendMessage($this->chatID, json_encode($formData), null); exit;
+                sendMessage($this->chatID, json_encode($applicationInfo), null); exit;
                 $registeredUser = $this->hrLinkApiProvider->registerDocumentApplication($this->user, $bossPhysicalId['physical_id'], $applicationInfo['hrlink_application_id']);
 
                 exit;
