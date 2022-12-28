cad595a527bd063a060e9c9a9c5dbd15af7d551a
booogie-man-07 <booogie.man.07@gmail.com>
2022-12-28 17:26:54 +0300

    Diall HRLink integration.

--- HrLinkApiProvider.php
+++ HrLinkApiProvider.php
@@ -56,7 +56,6 @@ class HrLinkApiProvider {
                         'templateSystemFields' => $templateSystemFields,
                         'templateFields' => $templateFields
                     );
-                    return $body;
                     $encodedBody = json_encode($body);
 
                     $curl = curl_init();
