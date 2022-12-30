c674a5132a21bd2976ba91823d2c5f34ab9e2738
booogie-man-07 <booogie.man.07@gmail.com>
2022-12-30 14:57:30 +0300

    Diall authorization vulnerability closed.

--- NonFinishedAuthorizationUserScenario.php
+++ NonFinishedAuthorizationUserScenario.php
@@ -62,6 +62,7 @@ class NonFinishedAuthorizationUserScenario {
                             if ($this->authroute->checkLogin($text)) {
                                 $result = $this->access->getUserByPersonnelNumber($text);
                                 if ($result) {
+                                    sendMessage($this->chatID, "Result 2: ".json_encode($result), null); exit;
                                     if ($result['is_employee'] == $this->constants['isNotEmployee']) {
                                         if($this->authroute->couldBeAuthorized($result)) {
                                             if ($this->authroute->comparse($text, $result['email'])) {
