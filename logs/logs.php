0a30fbf28123ae80c6f8801c1359492781d51630
booogie-man-07 <booogie.man.07@gmail.com>
2023-03-02 12:32:18 +0300

    DmsList daily update job added.

--- logs/logs.php
+++ logs/logs.php
@@ -45,7 +45,7 @@ class logs {
         $current_date_time = $logics->getDateForLogging();
         $logMessage = "[".(string)$current_date_time."];$message;$email\n";
 
-        file_put_contents('logs/upload_log_'.$current_date.'.txt', $logMessage, FILE_APPEND | LOCK_EX);
+        file_put_contents('logs/dms_upload_log_'.$current_date.'.txt', $logMessage, FILE_APPEND | LOCK_EX);
     }
 
 	function logEmptyUser($userId, $firstname, $lastname, $fullname, $form_fullname, $position, $email, $office_number, $internal_number, $mobile_number, $company_name, $company_id, $boss, $boss_position, $main_holliday_counter, $additional_holliday_counter, $activity) {
