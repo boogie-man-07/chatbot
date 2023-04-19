c7e56649276516f5e2341c2e8449ec050544b018
booogie-man-07 <booogie.man.07@gmail.com>
2023-04-19 20:12:47 +0300

    AD activation added.

--- AuthorizedUserScenario.php
+++ AuthorizedUserScenario.php
@@ -178,15 +178,14 @@ class AuthorizedUserScenario {
                 $this->mainInformationRoute->triggerActionForProceedOtherFeedback($this->chatID, $this->user['firstname']);
                 exit;
             case $this->commands['unlockAccount']:
-                sendMessage($this->chatID, strtok($this->user['email'], '@'), null); exit;
-                /*$activationResult = $this->adApiProvider->activate($this->user['email']);
+                $activationResult = $this->adApiProvider->activate($this->user['email']);
                 if ($activationResult['result']) {
                     sendMessage($this->chatID, 'Ваша учетная запись разблокирована.', null);
                     exit;
                 } else {
                     sendMessage($this->chatID, 'Ошибка разблокировки учетной записи.', null);
                     exit;
-                }*/
+                }
             case $this->commands['salaryInformation']:
                 $this->access->setState($this->chatID, $this->states['salaryState']);
                 $this->access->addAnalytics($this->user['user_id'], $this->analyticsTypes['DESTINATION'], $text);
