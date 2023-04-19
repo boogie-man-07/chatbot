5061e5a56b747d724fe58bd72ce52a6f7a33c5f7
booogie-man-07 <booogie.man.07@gmail.com>
2023-04-19 13:01:46 +0300

    HotRoute added.

---
+++ routes/hot/HotRoute.php
@@ -0,0 +1,37 @@
+<?php
+
+# Created by Murad Adygezalov
+# Date: 19.04.2023
+# Time: 12:46
+
+class HotRoute {
+
+    var $constants = null;
+    var $keyboards = null;
+
+    function __construct($constants, $keyboards) {
+        $this->constants = $constants;
+        $this->keyboards = $keyboards;
+    }
+
+    function isHotPhrase($text) {
+        return $text === mb_strtolower('отпуск');
+    }
+
+    function proceedIfHotDialog($chatID, $text) {
+        if ($this->isHotPhrase($text)) {
+            $phrase = mb_strtolower($text);
+            switch ($phrase) {
+                case 'отпуск':
+                    $this->triggerActionForGetMyVacationInformation($chatID);
+                    exit;
+            }
+        }
+    }
+
+    function triggerActionForGetMyVacationInformation($chatID) {
+        $reply = $this->constants->getVacationInformationText();
+        $keyboard = $this->keyboards->getVacationInformationKeyboard();
+        sendMessage($chatID, $reply, $keyboard);
+    }
+}
