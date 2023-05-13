0581c7c92562e4c70f25ee9565c4445b6dcd5402
booogie-man-07 <booogie.man.07@gmail.com>
2023-05-13 11:46:13 +0300

    Unlock logic.

--- providers/ADApiProvider.php
+++ providers/ADApiProvider.php
@@ -10,8 +10,8 @@ class ADApiProvider {
             CURLOPT_URL => "http://office.diall.ru:9082/unlock.php?name=$login",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 300,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_CONNECTTIMEOUT = 10,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "GET",
             CURLOPT_HTTPHEADER => array(
