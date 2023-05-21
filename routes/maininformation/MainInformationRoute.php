06a842c94b1f918dff334a6a1f1e4b97660644ee
booogie-man-07 <booogie.man.07@gmail.com>
2023-05-21 23:00:03 +0300

    Feedback email subject text added and formatting removed.

--- routes/maininformation/MainInformationRoute.php
+++ routes/maininformation/MainInformationRoute.php
@@ -85,6 +85,6 @@ class MainInformationRoute {
     function removeFormats($prefix, $text) {
         $prefix = str_replace(array("\r\n", "\n", "\r", "\t", " "), ''. $prefix);
         $text = str_replace(array("\r\n", "\n", "\r", "\t"), ''. $text);
-        return mb_substr($prefix.$text, 0, 150);
+        return substr($prefix.$text, 0, 150);
     }
 }
