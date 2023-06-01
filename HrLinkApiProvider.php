bfe914285aceb9b5950ede1a7a040bc2fca67806
booogie-man-07 <booogie.man.07@gmail.com>
2023-06-01 11:07:10 +0300

    HRLink applications V2.

--- HrLinkApiProvider.php
+++ HrLinkApiProvider.php
@@ -255,7 +255,7 @@ class HrLinkApiProvider {
                     $applicationEmployeeApproverExternalId = null;
                     $participantsFields = $this->generateApplicationsParticipantsFields($masterToken, $applicationEmployeeId, $applicationEmployeeApproverId, $bossRouteInfo['userBossPhysicalId']);
 
-                    return json_encode($bossRouteInfo);
+                    return json_encode($participantsFields);
 
                     $applications = array(
                         array(
